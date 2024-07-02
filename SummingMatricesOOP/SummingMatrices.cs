ebfe9b6830ebc8f1160fef023b0239f7ebf6d407
Pukl4 <140951563+Pukl4@users.noreply.github.com>
2024-07-02 18:47:24 +0300

    Getting the sum of matrices

---
+++ SummingMatricesOOP/SummingMatrices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummingMatricesOOP
+{
+    public class SummingMatrices
+    {
+        public int[][] GetMatrixSum(int matrixRowCount, int matrixColumnCount, int[][] firstMatrix, int[][] secondMatrix)
+        {
+            int[][] finalMatrix = new int[matrixRowCount][];
+
+            for (int i = 0; i < matrixRowCount; i++)
+            {
+                var finalMatrixRow = new List<int>();
+                for (int j = 0; j < matrixColumnCount; j++)
+                {
+                    finalMatrixRow.Add(firstMatrix[i][j] + secondMatrix[i][j]);
+                }
+
+                finalMatrix[i] = [.. finalMatrixRow];
+            }
+
+            return finalMatrix;
+        }
+    }
+}
